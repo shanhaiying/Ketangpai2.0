6b884bdae947b864bc9ecb5099ef9b4371843d76
James <caterpillarous@yahoo.com>
2016-07-24 22:06:29 +0800

    to the public net

--- KeTangPai2.0/demo/src/main/java/james/com/demo/Data/URL.java
+++ KeTangPai2.0/demo/src/main/java/james/com/demo/Data/URL.java
@@ -1,6 +1,14 @@
 package james.com.demo.Data;
 
 public class URL {
+    public final static String URL_LOGIN_STUDENT = "http://123.206.29.111/check/";
+    public final static String URL_REGISTER_STUDENT = "http://123.206.29.111/register/";
+    public final static String URL_LOGIN_TEACHER = "http://123.206.29.111/check_teacher/";
+    public final static String URL_CREATE_CLASS = "http://123.206.29.111/create_course/";
+    public final static String URL_SAVE_PROFILE = "http://123.206.29.111/save_profile/";
+    public final static String URL_PICK_COURSE = "http://123.206.29.111/pick_course/";
+    public final static String URL_GET_COURSE = "http://123.206.29.111/get_course/";
+    public final static String URL_GET_COURSE_TEACHER = "http://123.206.29.111/get_course_teacher/";
     /*
     //寝室IP
     public final static String URL_LOGIN_STUDENT = "http://10.2.220.24:8000/check/";
@@ -13,6 +21,7 @@ public class URL {
 
 
     //411教室IP
+    /*
     public final static String URL_LOGIN_STUDENT = "http://10.3.116.146:8000/check/";
     public final static String URL_REGISTER_STUDENT = "http://10.3.116.146:8000/register/";
     public final static String URL_LOGIN_TEACHER = "http://10.3.116.146:8000/check_teacher/";
@@ -21,6 +30,7 @@ public class URL {
     public final static String URL_PICK_COURSE = "http://10.3.116.146:8000/pick_course/";
     public final static String URL_GET_COURSE = "http://10.3.116.146:8000/get_course/";
     public final static String URL_GET_COURSE_TEACHER = "http://10.3.116.146:8000/get_course_teacher/";
+    *
 /*
     //郝老师办公室IP
     public final static String URL_LOGIN_STUDENT = "http://192.168.1.4:8000/check/";
